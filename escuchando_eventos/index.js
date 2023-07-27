4d0e19e8ff75f0a319781d55727ec26d837a31fb
Luis Carlos Bravo <luiscbravo94@gmail.com>
2023-07-27 15:33:37 +0100

    aprendemos a utilizar el addEventListener

--- escuchando_eventos/index.js
+++ escuchando_eventos/index.js
@@ -1,22 +1,48 @@
 const h1 = document.querySelector('#title')
+const form = document.querySelector('#formulario')
 const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
 const boton = document.querySelector('#btnCalcular');
 const spanSuma = document.querySelector('#span-suma')
+const demo = document.querySelector('#demo')
+const random = document.querySelector('#random')
+const handler = document.getElementById('remove-handler')
+form.addEventListener('submit',sumarInputs)
+handler.addEventListener('click',myFuntion)
 
-console.log(input1.value)
-function buttonOnClick(){
+function sumarInputs(event){
+    // console.log({event});
+    event.preventDefault()// para enviar que el formulario haga el efecto enviado-reiniciado
     
-if (input1.value == '0') {
-    console.log(input1.value);
-    input1.setAttribute('value','0')
-}else if(input2.value == '0')
-    input2.setAttribute('value','0')
-        
-    console.log(input1.value);  
     let suma = parseInt(input1.value) + parseInt(input2.value)
     let resultado = `La suma es ${suma}`
     spanSuma.innerHTML = resultado
 }
 
-// boton.addEventListener('click',calculo(input1,input2))
+document.addEventListener('mouseover',mousedOver)
+document.addEventListener('mouseout',mousedOut)
+document.addEventListener('click',click)
+document.addEventListener('mouseover',random_fuction)
+
+function mousedOver() {
+    demo.innerHTML= 'Moused over!'
+}
+//para que cuando salga el mouse cambie el valor de demo
+function mousedOut() {
+    demo.innerHTML = 'Moused out!'
+    document.body.style.backgroundColor = 'white'
+}
+//cuando se genere un click haga se relice el evento
+function click() {
+    demo.innerHTML = 'Clicked!'
+    document.body.style.backgroundColor = 'blue'
+}
+
+function random_fuction() {
+    random.innerHTML = Math.random()
+}
+
+
+function myFuntion() {
+    document.removeEventListener('mouseover',random_fuction)
+}
